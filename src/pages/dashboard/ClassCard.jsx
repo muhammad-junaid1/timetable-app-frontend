1528d64163a6fe30c130c1495788f4afb2a19729
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-11-30 12:29:20 +0500

    updated

--- src/pages/dashboard/ClassCard.jsx
+++ src/pages/dashboard/ClassCard.jsx
@@ -4,7 +4,9 @@ const ClassCard = ({ data = {} }) => {
     <div
       className={`flex justify-center floating shadow-2xl items-center flex-col py-16 rounded-[6px] class-card`}
     >
-      <div className="bg-[#ffffff] rounded px-2.5 text-black text-sm ">C405</div>
+      <div className="bg-[#ffffff] py-0.5 rounded px-2.5 text-black text-sm ">
+        C405
+      </div>
       <h1 className="my-2">{data?.title}</h1>
       <p>{data?.slot}</p>
     </div>
